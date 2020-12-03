3d206326015115582bc1feec938612d15cf72785
inessa5 <61697007+inessa5@users.noreply.github.com>
2020-12-03 19:49:33 +0500

    Update Contents.swift

--- l5_MinnikhanovaInessa.playground/Contents.swift
+++ l5_MinnikhanovaInessa.playground/Contents.swift
@@ -1,4 +1,4 @@
- enum Stamp : String{
+enum Stamp : String{
     case Audi="Audi", Peugeot="Peugeot", Volvo="Volvo"
  }
 
@@ -14,9 +14,9 @@ enum action: String{
 
     func printAll()  
  }
- 
+
  extension Car {
-    
+
 mutating func startEngine(){
          start=true          
         print("Автомобиль "+self.stamp.rawValue+" запущен ")
@@ -26,7 +26,7 @@ mutating func startEngine(){
          start=false          
         print("Автомобиль "+self.stamp.rawValue+" остановлен ")
      }
-      
+
   mutating   func openWindow(){
          self.openWindows=true          
         print("В автомобиле "+self.stamp.rawValue+" окна открыты ")
@@ -39,7 +39,7 @@ mutating func startEngine(){
      func printAll()  {
         print("Автомобиль "+self.stamp.rawValue+", год " + self.year.description)
     }
-    
+
   //mutating func initial(stamp: Stamp, year: Int, start: Bool, openWindows: Bool) {
   //      self.stamp = stamp;
   //      self.year = year;
@@ -56,17 +56,19 @@ class SportCar: Car {
      var year: Int 
      var start: Bool 
      var openWindows: Bool 
-    init(stamp: Stamp, year: Int, start: Bool, openWindows: Bool) {
+     //var description: String
+        init(stamp: Stamp, year: Int, start: Bool, openWindows: Bool) {
         self.stamp = stamp;
         self.year = year;
         self.start = start;
         self.openWindows = openWindows;
+        //self.description=""
     };
-    
+
      func printAll() {}
 
  }
- 
+
  //extension SportCar: ConsolePrintable {} 
  
  class TruckCar: Car {
@@ -76,7 +78,7 @@ class SportCar: Car {
      var openWindows: Bool 
     var volume: Int
      var filledVolume: Int
-
+    //var description: String
     init(stamp: Stamp, year: Int, start: Bool, openWindows: Bool, volume: Int) {
         self.volume = volume
         self.filledVolume = 0
@@ -85,55 +87,57 @@ class SportCar: Car {
         self.year = year;
         self.start = start;
        self.openWindows = openWindows;
-        
+        //self.description=""
     }
-    
+
     func loadCargo(){
          self.filledVolume=self.filledVolume+1
          print("Автомобиль "+self.stamp.rawValue+" добавил груз ")
 
-         
+
      }
      func unloadCargo(){
           self.filledVolume = self.filledVolume-1
           print("Автомобиль "+self.stamp.rawValue+" разгрузил " )
 
          }
-     
+
     func printAll() {}
-     
+
  }
- 
- protocol ConsolePrintable: CustomStringConvertible{
-    func printDescription()
-}
 
-extension ConsolePrintable{                        // расширяем протокол
-    func printDescription() {                     // реализуем метод для вывода в консоль поля description
-        print(description)                       // само поле description определено в CustomStringConvertible
+
+
+extension TruckCar: CustomStringConvertible{                        // расширяем протокол
+    var description: String{
+        return ""//"Автомобиль "+self.stamp.rawValue+", год " + self.year.description    // определяем, что именно будет выводиться
     }
+
 }
 
-extension SportCar: ConsolePrintable{                        // расширяем протокол
-    func printDescription() {                     // реализуем метод для вывода в консоль поля description
-        //print(description)                       // само поле description определено в CustomStringConvertible
+extension SportCar: CustomStringConvertible{ 
+      var description: String{
+        return "";//"Автомобиль \(stamp) год \(year)"    // определяем, что именно будет выводиться
     }
+    
 }
 
- 
+
  var sportCar1 = SportCar(stamp: .Audi, year: 2014, start: false, openWindows: false)
  var sportCar2 = SportCar(stamp: .Volvo, year: 2015, start: false, openWindows: false)
  var truckCar1 = TruckCar(stamp: .Peugeot, year: 2019, start: false, openWindows: false, volume: 2000)
  var truckCar2 = TruckCar(stamp: .Volvo, year: 2010, start: false, openWindows: false, volume: 100000)
 
- sportCar1.closeWindow()
- sportCar2.startEngine()
- truckCar1.loadCargo()
- truckCar1.loadCargo()
- truckCar2.startEngine()
- truckCar2.openWindow()
+ //sportCar1.closeWindow()
+ //sportCar2.startEngine()
+ //truckCar1.loadCargo()
+// truckCar1.loadCargo()
+// truckCar2.startEngine()
+ //truckCar2.openWindow()
 
-sportCar1.printAll()
-sportCar2.printAll()
-truckCar1.printAll()
-truckCar2.printAll()
+sportCar1.print()
+//sportCar2.printAll()
+//truckCar1.printAll()
+//truckCar2.printAll() 
+
+
